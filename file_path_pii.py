3f20c75ce0723c01017aa67936014e6bdc368e37
ShellyYBlack <74626165+ShellyYBlack@users.noreply.github.com>
2024-07-08 13:51:50 -0400

    Added removeprefix in file_path_pii.py

--- file_path_pii.py
+++ file_path_pii.py
@@ -15,7 +15,7 @@ for stringpattern in piilist:
     for f in filepathlist:
         result = re.search(regex, f)
         if not result == None:
-            for x in zip([result.string], [result.group()]):
+            for x in zip([result.string.removeprefix('/logical/')], [result.group()]):
                 resultlist.append(x)
             with open('/reports/file_path_pii.csv', mode='w') as result_file:
                 writer = csv.writer(result_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
